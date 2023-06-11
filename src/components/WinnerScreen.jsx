38a71a8e12f178dd8748b81a922d0f0e51ba6c15
Karen Ramirez <karen_ramirez82181@elpoli.edu.co>
2023-06-11 16:33:39 -0500

    Se agrega el css del mensaje final

--- src/components/WinnerScreen.jsx
+++ src/components/WinnerScreen.jsx
@@ -1,13 +1,25 @@
 import UsersContext from "../context/UserContext";
 import { useContext } from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 const WinnerScreen = () => {
-    const { winner } = useContext(UsersContext);
+  const { winner } = useContext(UsersContext);
   return (
     <div>
-        {winner === '' ? <p>No ha ganado nadie, se han acabado las oportunidades</p> : <p>{`${winner}`}</p>}
+      {winner === "" ? (
+        <Alert severity="error">
+          <AlertTitle>Game Over</AlertTitle>
+          No ha ganado nadie, <strong>se han acabado las oportunidades.</strong>
+        </Alert>
+      ) : (
+        <Alert severity="success">
+          <AlertTitle>Winner</AlertTitle>
+          <strong>{`${winner}`}</strong>
+        </Alert>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default WinnerScreen
+export default WinnerScreen;
